6478a6a2d3142302f232c5437b12308f54e66289
congshuanglong <Administrator@congsl.ailk.com>
2013-06-01 21:17:08 +0800

    move comments to README.md from rb file

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ alipay
 ======
 
 支付宝的支付接口包
+======
+
+Alipay（支付宝)库文件,使用方法如下:
+alipay_uri = Alipay::Merchant.new do |m|
+  m.return_url = 'hello'
+  m.show_url='http://www.baidu.com'
+  m.subject = 'goodsName'
+  m.body = 'theBodyOfTheGoods'
+  m.out_trade_no = '20091009143121'
+  m.price = 20.34
+  m.quantity = 4
+end.uri
+
--- merchant.rb
+++ merchant.rb
@@ -1,17 +1,6 @@
 require 'iconv'
 require 'net/http'
 require 'uri'
-#Alipay（支付宝)库文件
-# ===使用方法如下:
-# * alipay_uri = Alipay::Merchant.new do |m|
-# * m.return_url = 'hello'
-# * m.show_url='http://www.baidu.com'
-# * m.subject = 'goodsName'
-# * m.body = 'theBodyOfTheGoods'
-# * m.out_trade_no = '20091009143121'
-# * m.price = 20.34
-# * m.quantity = 4
-# * end.uri
 module Alipay
   class Merchant
     GATEWAY='http://www.alipay.com/cooperate/gateway.do'
